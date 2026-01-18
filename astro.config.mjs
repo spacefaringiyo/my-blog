7317d02e55cef4ea6da3aed30219a5d00cb7ca26
spacefaringiyo <sliba908@gmail.com>
2026-01-19 04:13:17 +0900

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	// 1. Your actual website URL (instead of example.com)
+	site: 'https://spacefaringiyo.github.io',
+	// 2. The sub-folder where your blog lives
+	base: '/my-blog',
+	
 	integrations: [mdx(), sitemap()],
 });
